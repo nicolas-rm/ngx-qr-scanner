3af5192b61c94e8bb35a56a6214f14f77f4af10d
NICOLAS RM <nicolas.rm540@gmail.com>
2024-06-07 12:35:39 -0600

    Scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,16 @@ import { AppComponent } from './app.component';
 import { SafePipe } from './safe.pipe';
 import { FormsModule } from '@angular/forms';
 import { LOAD_WASM, NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+import { QrComponent } from './qr/qr.component';
 
 LOAD_WASM().subscribe((res: any) => {
-  console.log('LOAD_WASM',res)
-})
+    console.log('LOAD_WASM', res);
+});
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SafePipe
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    NgxScannerQrcodeModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, SafePipe, QrComponent],
+    imports: [BrowserModule, FormsModule, NgxScannerQrcodeModule],
+    providers: [],
+    bootstrap: [AppComponent, QrComponent],
 })
-export class AppModule { }
+export class AppModule {}
